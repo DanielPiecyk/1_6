ada9f8d8a5b00b98bb5ceb556d5ce6eab23a1f9f
danielpiecyk <100698293+DanielPiecyk@users.noreply.github.com>
2022-03-15 14:19:15 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,12 @@
+class Main {
+  public static void main(String[] args) {
+    int j;
+    for(int i=0;i<=300;i=i+20)
+      {
+    System.out.print(i+" [C]");
+        j=i*2+32;
+        System.out.print(j+" [F]\n");
+        
+      }
+  }
+}
